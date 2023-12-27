e0401a927374daa1f734de5e367867a7ca65e9c9
Afzalzakirhussain <afzalzakirhussain@gmail.com>
2023-12-28 00:53:42 +0530

    basic folders added

---
+++ frontend/react/src/components/demo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const demo = () => {
+    return (
+        <div>demo</div>
+        // only for demo you can delete this
+    )
+}
+
+export default demo
---
+++ frontend/react/src/constants/index.js
@@ -0,0 +1,1 @@
+export const DEMO_TEXT = "DEMO_TEXT"
